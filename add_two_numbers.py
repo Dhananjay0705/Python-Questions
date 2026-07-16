f8e45e0ff73ac880e5de8a06d2c9d3fdff91181c
Dhananjay Kumar Sharma <ds9991841@gmail.com>
2026-07-16 12:10:28 +0530

    Create add_two_numbers.py

---
+++ add_two_numbers.py
@@ -0,0 +1,13 @@
+# # solution 1) pre defined variables
+
+num1 = 30
+num2 = 20
+print("The sum of given numbers is : ", num1 + num2)
+
+
+## Solution 2) With user input
+
+num3 = float(input("Enter first Number : "))
+num4 = float(input("Enter Second Number : "))
+
+print("The sum of given numbers is : ", num3 + num4)
