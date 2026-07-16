b27d704a4ea0e63275cf57d1dedd67e932207f34
Dhananjay Kumar Sharma <ds9991841@gmail.com>
2026-07-16 14:00:37 +0530

    Update calculate_squareroot.py

--- calculate_squareroot.py
+++ calculate_squareroot.py
@@ -6,3 +6,14 @@ num1 = float(input("Enter a number to find the square root : "))
 sr = num1**(1/2)
 print("Square root of the given number is : ",sr)
 
+## Solution 2) Using math module
+
+import math ## math = Module
+
+num1 = float(input("Enter a number to find the square root : "))
+
+sr = math.sqrt(num1)
+
+print("Square root of the given number is : ",sr)
+
+ 
